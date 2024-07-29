efce8b20b22356c507376a9d0940a51a726a8a09
Rafaelenarvaez <100246546+Rafaelenarvaez@users.noreply.github.com>
2024-07-29 08:24:01 -0400

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,10 +74,8 @@ export default function Home() {
       apartment.description.toLowerCase().includes(searchText)
     );
   });
-  console.log(filteredApartments);
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 container">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md mb-4 flex">
         <input
           type="text"
@@ -98,16 +96,16 @@ export default function Home() {
           <p className="text-gray-500">Cargando apartamentos</p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center px-4 py-4">
           {filteredApartments.map(apartment => (
             <Link key={apartment.id} href={`/pages/apartment/${apartment.id}`} passHref>
-              <div className="w-full max-w-xs bg-white shadow-lg rounded-lg overflow-hidden mb-4">
+              <div className="w-full max-w-xs bg-white shadow-lg rounded-lg overflow-hidden mb-6 mx-2">
                 {apartment.rooms.length > 0 ? (
                   <Slider {...settings}>
                     {apartment.rooms.map((room) => (
                       <div key={room.id} className="carousel-item">
                         <img
-                          className="w-full h-full object-cover"
+                          className="w-full h-48 object-cover"
                           src={room.image_url}
                           alt={room.name}
                         />
